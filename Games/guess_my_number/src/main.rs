1b29df4f8162a47735c39bafd9dba879f5cfdcc9
The-Green-Gamer <daniel.gladman1@gmail.com>
2022-07-03 11:44:10 +0100

    Guess (1-1000)

--- Games/guess_my_number/src/main.rs
+++ Games/guess_my_number/src/main.rs
@@ -1,11 +1,11 @@
 use std::{io};
 fn main() {
-    let my_number = fastrand::u16(..);
+    let my_number = fastrand::u16(1..1000);
     let mut correct = false;
     let mut user_number: u16;
 
     while correct == false {
-        println!("Guess my number (0-65535)");
+        println!("Guess my number (1-1000)");
 
         let mut user_guess = String::new();
 
@@ -23,14 +23,14 @@ fn main() {
 
         }
         else if user_number < my_number{
-            println!("Your guess is lower than my number");
+            println!("Too low");
 
         }
         else if user_number > my_number{
-            println!("Your guess is higher than my number");
+            println!("Too high");
 
         }
-        
+
     }
     println!("You Win!");
 
--- Games/guess_my_number/target/debug/deps/guess_my_number-86dbefa6162aeab9.d
+++ Games/guess_my_number/target/debug/deps/guess_my_number-86dbefa6162aeab9.d
@@ -1,5 +1,5 @@
-c:\Users\danie\Documents\Rust\Rust-Projects\Games\guess_my_number\target\debug\deps\guess_my_number-86dbefa6162aeab9.rmeta: src\main.rs
+e:\Users\danie\Documents\Rust\Rust-Projects\Games\guess_my_number\target\debug\deps\guess_my_number-86dbefa6162aeab9.rmeta: src\main.rs
 
-c:\Users\danie\Documents\Rust\Rust-Projects\Games\guess_my_number\target\debug\deps\guess_my_number-86dbefa6162aeab9.d: src\main.rs
+e:\Users\danie\Documents\Rust\Rust-Projects\Games\guess_my_number\target\debug\deps\guess_my_number-86dbefa6162aeab9.d: src\main.rs
 
 src\main.rs:
